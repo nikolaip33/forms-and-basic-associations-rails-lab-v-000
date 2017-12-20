a23326ca685d32b68e775284c74752c1f94330f7
Nikolai Pawlowski <nikolaip@gmail.com>
2017-12-20 12:07:00 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -3,7 +3,13 @@ class Song < ActiveRecord::Base
   belongs_to :genre
   has_many :notes
 
+  def genre_name
+    self.genre.name if genre
+  end
 
+  def genre_name=(name)
+    self.genre = Genre.find_or_create_by(name: name)
+  end
 
   def artist_name=(name)
     self.artist = Artist.find_or_create_by(name: name)
